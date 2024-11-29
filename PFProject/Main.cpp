a9db917033d3fece75c2ba6b563976707c4210e0
Muhammad Shahmeer <suno.anser@gmail.com>
2024-11-29 15:41:55 +0500

    Add project files.

---
+++ PFProject/Main.cpp
@@ -0,0 +1,14 @@
+// this is the start of programming fundamental semester project.
+// Muhammad Shahmeer(24F-0684)
+// Sobaan Ahmed(24F-0564)
+// Hafiza Arfa (24F-0721)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "HelloWorld!";
+	system("pause");
+	return 0;
+}
