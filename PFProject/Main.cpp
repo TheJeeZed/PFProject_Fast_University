8b7308e21a14b3b8fee9df453f84274ac7f8012e
Muhammad Shahmeer <suno.anser@gmail.com>
2024-12-06 19:14:00 +0500

    Made user Enter his name for future highscore storage . will work on character spawning, anyone can handle the obstacle spawing code?

--- PFProject/Main.cpp
+++ PFProject/Main.cpp
@@ -4,10 +4,22 @@
 // Hafiza Arfa (24F-0721)
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-const int ROW = 50, COL = 50;
+const int ROW = 30, COL = 50;
+
+string nameInput() {
+	string name = "";
+	cout << "Enter your name(8 character long with no spaces):";
+	cin >> name;
+	if (name.size() > 8) {
+		return name.substr(0, 8);
+	}
+	return name;
+}
+
 void placeWalls(char arr[][COL]) {
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
@@ -33,10 +45,10 @@ void printMap(char arr[][COL]) {
 }
 
 int main() {
+	string name = "";
 	char map[ROW][COL];
 	placeWalls(map);
 	printMap(map);
-	cout << "HelloWorld!";
 	system("pause");
 	return 0;
 }
