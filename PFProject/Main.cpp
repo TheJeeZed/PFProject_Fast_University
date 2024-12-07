9603db606febbd4386a54bac98897d1b46c2d070
Muhammad Shahmeer <suno.anser@gmail.com>
2024-12-07 11:15:21 +0500

    Work done on file handling system for highscores.

--- PFProject/Main.cpp
+++ PFProject/Main.cpp
@@ -5,10 +5,14 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
 
 using namespace std;
 
-const int ROW = 30, COL = 50;
+const int ROW = 30, COL = 30, SCORES = 10;
+int highscores[SCORES] = { 0 };
+string names[SCORES] = { "NA","NA","NA","NA","NA","NA","NA","NA","NA","NA"};
 
 string nameInput() {
 	string name = "";
@@ -19,31 +23,81 @@ string nameInput() {
 	}
 	return name;
 }
-
-void placeWalls(char arr[][COL]) {
+//Put Scores back to the text file.
+bool putScores() {
+	ofstream fout;
+	fout.open("highscores.txt", ios::out);
+	if (fout.fail()) {
+		return false;
+	}
+	for (int i = 0; i < SCORES; i++) {
+		fout << names[i];
+		fout << " ";
+		fout << highscores[i];
+	}
+	fout.close();
+	return true;
+}
+//Read Scores from the text file.
+bool getScores() {
+	ifstream fin;
+	fin.open("highscores.txt", ios::in);
+	if (fin.fail()) {
+		return false;
+	}
+	for (int i = 0; i < SCORES ;i++) {
+		fin >> names[i];
+		fin >> highscores[i];
+	}
+	fin.close();
+	return true;
+}
+//Update the score keeping the relative position same
+bool updateScores(int score,string name) {
+	if (score > highscores[SCORES - 1]) {
+		int insertindex = SCORES - 1;
+		for (int i = insertindex - 1; i >= 0; i--) {
+			if (score > highscores[i]) {
+				insertindex = i;
+			}
+		}
+		for (int i = SCORES - 2; i >= insertindex ; i--) {
+			highscores[i + 1] = highscores[i];
+			names[i + 1] = names[i];
+		}
+		highscores[insertindex] = score;
+		names[insertindex] = name;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+//Places Walls Around the Perimeter
+void placeWalls(char map[][COL]) {
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
 			if (i == 0 || i == COL - 1) {
-				arr[i][j] = '-';
+				map[i][j] = '-';
 			}
 			else if (j == 0 || j == ROW - 1) {
-				arr[i][j] = '|';
+				map[i][j] = '|';
 			}
 			else {
-				arr[i][j] = ' ';
+				map[i][j] = ' ';
 			}
 		}
 	}
 }
-void printMap(char arr[][COL]) {
+//Prints the entire map
+void printMap(char map[][COL]) {
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
-			cout << arr[i][j];
+			cout << map[i][j];
 		}
 		cout << endl;
 	}
 }
-
 int main() {
 	string name = "";
 	char map[ROW][COL];
